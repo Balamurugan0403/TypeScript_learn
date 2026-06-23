16fd2d5881c4e9dd8bb3eaa21005650b044707a3
Balamurugan <murugan020325@gmail.com>
2026-06-23 10:50:45 +0530

    Refactor printing details to use command line arguments

--- Sequence_js/printingdetails.ts
+++ Sequence_js/printingdetails.ts
@@ -1,7 +1,7 @@
-let personName: string = "John";
-let age:number=30;
-let favoriteColor:string="Green";
+const name = process.argv[2];
+const age = process.argv[3];
+const color = process.argv[4];
 
-console.log(personName);
-console.log("Age:",age);
-console.log("Favorite Color:",favoriteColor);
+console.log(name);
+console.log("Age: " + age);
+console.log("Favorite Color: " + color);
